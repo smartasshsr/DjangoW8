313531c7c1a3f58bb5df2df3e52f0241163d0e87
JG-C3 <jgc3django@gmail.com>
2022-11-02 15:50:47 +0900

    Update Django W8

--- jgDjango/urls.py
+++ jgDjango/urls.py
@@ -14,18 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# [코드 추가] re_path 함수 추가로 불러오기
 from django.urls import path, include
-# [코드 작성] static 파일을 제공하기 위한 serve 함수 불러오기
-
-# [코드 작성] settings.py의 개체 가져오기
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('page/', include('page.urls')),
     path('game/', include('game.urls')),
 ]
-
-# [코드 작성] DEBUG가 False일 경우 static을 가져올 수 있도록 설정
-
